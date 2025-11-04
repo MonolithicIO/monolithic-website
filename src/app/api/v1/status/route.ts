06629a9d66cf4810c5a10a218f8d82d6e43baaf4
Alaksion <lbeurmann.dev@gmail.com>
2025-11-04 15:50:42 -0300

    feat: make compose read env file

--- src/app/api/v1/status/route.ts
+++ src/app/api/v1/status/route.ts
@@ -1,5 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
+import databasePool from "../../../../core/database";
 
 export async function GET(req: NextRequest) {
-  return NextResponse.json({ hello: "world" }, { status: 200 });
+  return NextResponse.json({ databaseHealth: checkDatabaseHealth }, { status: 200 });
+}
+
+async function checkDatabaseHealth(): Promise<boolean> {
+  const database = databasePool;
+
+  try {
+    const testQuery = await database.query("Select $1::text as message", ["Hello world"]);
+    return testQuery.rows.length > 0;
+  } catch {
+    return false;
+  }
 }
