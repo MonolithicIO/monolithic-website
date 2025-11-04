8970cf85f1c3ff4d67791cbda79fb9540abb3192
Alaksion <lbeurmann.dev@gmail.com>
2025-11-04 17:53:46 -0300

    feat: change jest configuration

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,9 @@
 import nextJest from "next/jest";
-import type { Config } from "jest";
 
 const createJestConfig = nextJest({
-  dir: "./",
+  dir: ".",
 });
 
-const config: Config = {
-  coverageProvider: "v8",
-  testEnvironment: "jsdom",
-};
-
-export default createJestConfig(config);
+export default createJestConfig({
+  moduleDirectories: ["node_modules", "<rootDir>"],
+});
