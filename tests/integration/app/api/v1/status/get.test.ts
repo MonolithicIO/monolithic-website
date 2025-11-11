c4f73dfdbe45536b56d70eb8aa0977b5dbe9472b
Alaksion <lbeurmann.dev@gmail.com>
2025-11-11 11:25:19 -0300

    feat: fix webserver <> jest race condition

--- tests/integration/app/api/v1/status/get.test.ts
+++ tests/integration/app/api/v1/status/get.test.ts
@@ -1,3 +1,5 @@
+import { before } from "node:test";
+
 test("GET api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
