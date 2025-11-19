5de4e555b3296dc9a5ce674cb2ce5eafe76f288a
Alaksion <lbeurmann.dev@gmail.com>
2025-11-18 21:24:38 -0300

    feat: setup firebase environments

---
+++ src/core/firebase/config.ts
@@ -0,0 +1,12 @@
+import { initializeApp, FirebaseOptions, getApps, getApp } from "firebase/app";
+
+const firebaseConfig: FirebaseOptions = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
+
+const firebaseApp = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
+
+export default firebaseApp;
