0b9aad691f7a256ef883dfdf1739c39ea494e11a
Alaksion <lbeurmann.dev@gmail.com>
2025-11-24 17:35:21 -0300

    fix: multiple firebase instances on server side

--- src/core/firebase/firebase-server.config.ts
+++ src/core/firebase/firebase-server.config.ts
@@ -1,17 +1,8 @@
-import { credential } from "firebase-admin";
-import { initializeApp } from "firebase-admin/app";
+import admin from "firebase-admin";
 
 const isDevelopment = process.env.NODE_ENV === "development";
 
-const serverFirebaseApp = initializeApp({
-  credential: isDevelopment
-    ? credential.applicationDefault()
-    : credential.cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY,
-      }),
-});
+const serverFirebaseApp = admin.apps.length === 0 ? admin.initializeApp() : admin.app();
 
 if (isDevelopment) {
   process.env.FIRESTORE_EMULATOR_HOST = "localhost:8080";
