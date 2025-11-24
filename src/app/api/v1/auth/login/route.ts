eeff738b4590de086575369fd0d2778faab1264c
Alaksion <lbeurmann.dev@gmail.com>
2025-11-24 17:05:38 -0300

    feat: implement login endpoint

--- src/app/api/v1/auth/login/route.ts
+++ src/app/api/v1/auth/login/route.ts
@@ -1,9 +1,11 @@
 import { createHandler } from "@core/api/api-handler";
 import authMiddleware from "@core/api/middlewares/auth.middleware";
+import SignInService from "@services/sign-in.service";
 import { NextResponse } from "next/server";
 
 export const POST = createHandler([authMiddleware], async context => {
   const authToken = context.authToken;
-
-  return NextResponse.json({ hello: authToken }, { status: 200 });
+  const loginService = new SignInService();
+  const response = await loginService.signIn(authToken);
+  return NextResponse.json(response, { status: 201 });
 });
