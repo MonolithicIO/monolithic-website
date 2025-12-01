26d134b26b2d2fa8cb19f7165219f18e1ca29f33
Alaksion <lbeurmann.dev@gmail.com>
2025-12-01 16:57:17 -0300

    feat: implement /me endpoint

--- src/app/api/v1/user/me/route.ts
+++ src/app/api/v1/user/me/route.ts
@@ -1,7 +1,13 @@
 import { createHandler } from "@core/api/api-handler";
 import authMiddleware from "@core/api/middlewares/auth.middleware";
+import GetUserService from "@services/get-user.service";
 import { NextResponse } from "next/server";
 
 export const GET = createHandler([authMiddleware], async _context => {
-  return NextResponse.json({ message: "Hello, World!" }, { status: 200 });
+  const userService = new GetUserService();
+
+  const { userId } = _context;
+  const user = await userService.getUserById(userId);
+
+  return NextResponse.json({ user }, { status: 200 });
 });
