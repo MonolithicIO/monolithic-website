fe8841d02ce3ad714a47c5e645a552359d5618bd
Alaksion <lbeurmann.dev@gmail.com>
2025-12-02 16:30:02 -0300

    feat: remove endpoint

--- src/app/api/v1/auth/reset-password/route.ts
+++
@@ -1,6 +0,0 @@
-import { createHandler } from "@core/api/api-handler";
-import { NextResponse } from "next/server";
-
-export const POST = createHandler([], async _context => {
-  return NextResponse.json({ hello: "world" });
-});
